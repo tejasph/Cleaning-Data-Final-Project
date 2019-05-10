8413b5d7dc713de3ea8c7d81d2c49c50008cfacb
tejasph <tphaterp@gmail.com>
2019-05-09 18:24:10 -0700

    Adding different read functions to bring in all training file formats into a table form that we can then merge in the future.

--- run_analysis.R
+++ run_analysis.R
@@ -8,3 +8,9 @@
 #4. Appropriately label variables
 #5. Create a second, independent tidy data set w/ avg of each variable for each activity/subject
 
+
+#Reading in Training Data(set wd to Cleaninng-Data-Final-Project Folder )
+TrainX <- read.csv("UCI HAR Dataset/train/X_train.txt", header = F, sep = "")
+TrainY <- read.csv("UCI HAR Dataset/train/Y_train.txt", header = F, sep = "")
+TrainSubject <- read.csv("UCI HAR Dataset/train/subject_train.txt", header = F, sep = "")
+FeatureVector <- read.delim("UCI HAR Dataset/features.txt", header = F, sep = "")
