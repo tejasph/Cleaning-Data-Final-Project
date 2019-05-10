6db80da003b73ffc55f486707c696f0b50b2a947
tejasph <tphaterp@gmail.com>
2019-05-09 17:14:31 -0700

    Initial Project Set-up, including relevant data files

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+#Coursera Course 3 Final Project
+#run_analysis.R
+
+#Goals:
+#1. Merge test and training datasets
+#2. Extract measurements on mean and standard dev
+#3. Use descriptive names to label the data set
+#4. Appropriately label variables
+#5. Create a second, independent tidy data set w/ avg of each variable for each activity/subject
+
